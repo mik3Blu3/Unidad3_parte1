c2d61d565800235d1f863b8365f1b33c9ec04d76
mik3Blu3 <enanin12992@gmail.com>
2021-06-08 17:07:49 -0600

    Agregue el ejercicio 4 en el lenguaje Java

---
+++ ejercicio4.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class ejercicio4 {
+    static Scanner entrada = new Scanner(System.in);
+    public static void main(String args[]) 
+    {
+            System.out.println("Ingrese el numero al cual desea sacarle el cuadrado y el cubo");
+            int num = entrada.nextInt();
+            System.out.println("");
+             int numeroCuadrado = (num*num);
+             int numeroCubo = (num*num*num);
+            System.out.println("El cuadrado del numero "+num+ " es "+numeroCuadrado+" y el cubo del mismo es "+numeroCubo);  
+    }
+}
