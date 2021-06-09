080a7cf9976b87da7bd584c1f10cc6e59de49ec3
mik3Blu3 <enanin12992@gmail.com>
2021-06-08 18:19:23 -0600

    Agregue el ejercicio8 en lenguaje Java

---
+++ ejercicio8.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ejercicio8 {
+    static Scanner entrada = new Scanner(System.in);
+    public static void main (String args [])
+    {
+        System.out.println("Ingrese los tres numeros a sacar el promedio: ");
+        System.out.println("Ingrese el primer numero: ");
+        int num1 = entrada.nextInt();
+        System.out.println("Ingrese el segundo numero: ");
+        int num2 = entrada.nextInt();
+        System.out.println("Ingrese el tercer numero: ");
+        int  num3 = entrada.nextInt();
+        int promNum = (num1+num2+num3)/3;
+        System.out.println("El promedio de los tres numeros es: "+promNum);
+    }
+
+}
