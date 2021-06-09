ef85115cbeb2a3a4d802f51bbc4f67b35e22dcc5
mik3Blu3 <enanin12992@gmail.com>
2021-06-08 22:52:35 -0600

    Agregue el ejercicio15 en lenguaje Java

---
+++ ejercicio15.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ejercicio15 {
+    static Scanner entrada = new Scanner(System.in);
+    public static void main(String args[])
+    {
+        System.out.println("Ingrese cualquier numero:");
+        int num = entrada.nextInt();
+        if((num < 0) && (num != 0))
+        {   
+            System.out.println("EL numero "+num+" es NEGATIVO");
+        }else if((num > 0) && (num != 0) ){
+            System.out.println("El numero "+num+" es POSITIVO");
+        }else{
+            System.out.println("El numero "+num+" es NEUTRO");
+        }
+    }
+}
